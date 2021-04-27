3cadff4406a94787377c5d4c69829226fabdf0f5
koalagang <74791721+koalagang@users.noreply.github.com>
2021-04-27 18:29:54 +0100

    Fixed a mistake. Now variables should be shared across the scripts.

--- artix-installer.sh
+++ artix-installer.sh
@@ -14,46 +14,54 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     read -p "Retype host name: " CONFIRMHOST
     until [ "$HOST" = "$CONFIRMHOST" ]; do
         echo "Host names did not match!"
-        read -p "Enter your host name: " export HOST
-        read -p "Retype host name: " export CONFIRMHOST
+        read -p "Enter your host name: "  HOST
+        read -p "Retype host name: "  CONFIRMHOST
     done
     echo
     echo "Your host name is $HOST."
+    export HOST
     echo
-    read -p "Enter your username: " export USERNAME
-    read -p "Retype username: " export CONFIRMUSERNAME
+    read -p "Enter your username: "  USERNAME
+    read -p "Retype username: "  CONFIRMUSERNAME
     until [ "$USERNAME" = "$CONFIRMUSERNAME" ]; do
         echo "Usernames did not match!"
-        read -p "Enter your username: " export USERNAME
-        read -p "Retype username: " export CONFIRMUSERNAME
+        read -p "Enter your username: "  USERNAME
+        read -p "Retype username: "  CONFIRMUSERNAME
     done
     echo
     echo "Your username is $USERNAME."
+    export USERNAME
     echo
-    read -p "Enter your password: " export PASSWORD
-    read -p "Retype password: " export CONFIRMPASSWORD
+    read -p "Enter your password: "  PASSWORD
+    read -p "Retype password: "  CONFIRMPASSWORD
     until [ "$PASSWORD" = "$CONFIRMPASSWORD" ]; do
         echo "Passwords did not match!"
-        read -p "Enter your password: " export PASSWORD
-        read -p "Retype password: " export CONFIRMPASSWORD
+        read -p "Enter your password: "  PASSWORD
+        read -p "Retype password: "  CONFIRMPASSWORD
     done
-    echo""
+    echo
     echo "Your password is $PASSWORD."
-    echo""
-    read -p "Enter your root password: " export ROOTPASSWORD
-    read -p "Retype root password: " export ROOTCONFIRMPASSWORD
+    export PASSWORD
+    echo
+    read -p "Enter your root password: "  ROOTPASSWORD
+    read -p "Retype root password: "  ROOTCONFIRMPASSWORD
     until [ "$ROOTPASSWORD" = "$ROOTCONFIRMPASSWORD" ]; do
         echo "Root passwords did not match!"
-        read -p "Enter your root password: " export ROOTPASSWORD
-        read -p "Retype root password: " export ROOTCONFIRMPASSWORD
+        read -p "Enter your root password: "  ROOTPASSWORD
+        read -p "Retype root password: "  ROOTCONFIRMPASSWORD
     done
-    read -p "Are you dual-booting? [Y/N] " export DUAL
-    read -p "Confirm your answer by typing it again: " export CONFIRMDUAL
+    echo
+    echo "Your password is $ROOTPASSWORD."
+    export ROOTPASSWORD
+    echo
+    read -p "Are you dual-booting? [Y/N] "  DUAL
+    read -p "Confirm your answer by typing it again: "  CONFIRMDUAL
     until [ "$DUAL" = "$CONFIRMDUAL" ]; do
         echo "Answers did not match!"
-        read -p "Are you dual-booting? [Y/N] " export DUAL
-        read -p "Confirm your answer by typing it again: " export CONFIRMDUAL
+        read -p "Are you dual-booting? [Y/N] "  DUAL
+        read -p "Confirm your answer by typing it again: "  CONFIRMDUAL
     done
+    export DUAL
     mkfs.ext4 /dev/sda2 # root
     mkfs.ext4 /dev/sda3 # home
     mkfs.fat -F32 /dev/sda1 # boot
