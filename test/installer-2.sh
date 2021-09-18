06fdc616c33c5ef81651f79ce93b23a89819e220
koalagang <74791721+koalagang@users.noreply.github.com>
2021-09-18 19:20:21 +0100

    Fixed mistake which prevented you from configuring GRUB if you chose not to encrypt your drive.

--- test/installer-2.sh
+++ test/installer-2.sh
@@ -23,12 +23,16 @@ pacman -Syy networkmanager networkmanager-runit grub efibootmgr xorg --noconfirm
 # there's no need to install amd-ucode manually for amd users because it comes as a part of linux-firmware but intel-ucode does not so we need to manually install that for intel users
 [[ "$(lscpu | grep 'Model name:')" =~ 'Intel' ]] && pacman -S intel-ucode --noconfirm
 
-[ -n "$encrypt" ] && echo 'Configuring mkinitcpio...' && pacman -S lvm2 cryptsetup --noconfirm && cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak && cp /etc/default/grub /etc/default/grub.bak &&
+[ -n "$encrypt" ] &&
+    echo 'Configuring mkinitcpio...' && pacman -S lvm2 cryptsetup --noconfirm && cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak && cp /etc/default/grub /etc/default/grub.bak &&
     sed -i "s+$(grep '^HOOKS' /etc/mkinitcpio.conf)+HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard lvm2 fsck)+" /etc/mkinitcpio.conf &&
-    mkinitcpio -p "$KERNEL" && echo 'Successfully configured mkinitcpio!' && echo 'Configuring grub...' && cp /etc/default/grub /etc/default/grub.bak &&
-    sed -i -e "s+$(grep -w 'GRUB_CMDLINE_LINUX' /etc/default/grub)+GRUB_CMDLINE_LINUX=\"cryptdevice=/dev/sda2:cryptlvm\"+" -e "s+$(grep 'GRUB_ENABLE_CRYPTODISK' /etc/default/grub)+GRUB_ENABLE_CRYPTODISK=y+" /etc/default/grub &&
+    mkinitcpio -p "$KERNEL" && echo 'Successfully configured mkinitcpio!' &&
+    echo 'Configuring grub...' && cp /etc/default/grub /etc/default/grub.bak &&
+    sed -i -e "s+$(grep -w 'GRUB_CMDLINE_LINUX' /etc/default/grub)+GRUB_CMDLINE_LINUX=\"cryptdevice=/dev/sda2:cryptlvm\"+" \
+    -e "s+$(grep 'GRUB_ENABLE_CRYPTODISK' /etc/default/grub)+GRUB_ENABLE_CRYPTODISK=y+" /etc/default/grub
+
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub --recheck "$DEVICE" &&
-grub-mkconfig -o /boot/grub/grub.cfg && echo 'Successfully configured grub!'
+grub-mkconfig -o /boot/grub/grub.cfg && echo 'Successfully installed grub!'
 
 echo "$HOST" > /etc/hostname
 printf '127.0.0.1 \t localhost\n::1 \t\t localhost\n127.0.1.1 \t %s.localdomain \t %s' "$HOST" "$HOST" > /etc/hosts
