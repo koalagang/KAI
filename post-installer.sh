29dcdd0f2471b1e18dc360323e2fc139d9bdf072
koalagang <74791721+koalagang@users.noreply.github.com>
2022-02-02 18:02:39 +0000

    Corrected some mistakes regarding the Arch repos.

--- post-installer.sh
+++ post-installer.sh
@@ -4,20 +4,20 @@
 sudo pacman git curl base base-devel -Syu --noconfirm --needed
 
 # install paru (using baph so that we don't have to compile it)
-curl -sL 'https://raw.githubusercontent.com/PandaFoss/baph/master/baph' -o "$HOME/baph" && chmod +x baph
+curl -sL 'https://raw.githubusercontent.com/PandaFoss/baph/master/baph' -o baph && chmod +x baph
 ./baph -nN -i paru-bin
-rm "$HOME/baph"
+rm baph
 
 # configure pacman to support lib32 and have parallel downloads and pretty colours ;)
 sudo cp --backup=numbered /etc/pacman.conf /etc/pacman.conf.bak && echo '/etc/pacman.conf has been safely backed up!'
-sudo sed -i 's/#[lib32]/[lib32]/' /etc/pacman.conf && grep -A1 -n '\[lib32\]' /etc/pacman.conf | tail -1 | cut -d'-' -f1 | xargs -I% sudo sed -i '%s/#//' /etc/pacman.conf
+sudo sed -i 's/#\[lib32\]/\[lib32\]/' /etc/pacman.conf && grep -A1 -n '\[lib32\]' /etc/pacman.conf | tail -1 | cut -d'-' -f1 | xargs -I% sudo sed -i '%s/#//' /etc/pacman.conf
 sudo pacman -Syu artix-archlinux-support lib32-artix-archlinux-support --noconfirm && sudo pacman-key --populate archlinux
-cat arch-repos.txt | sudo ted -a /etc/pacman.conf >/dev/null
+cat arch-repos.txt | sudo tee -a /etc/pacman.conf >/dev/null
 sudo sed -i -e "s/#ParralelDownloads = 5/ParallelDownloads = 20/" -e 's/#Color/Color/' /etc/pacman.conf
 
 # install all my packages
 readarray -t progs < 'progs.txt'
-paru -S "${progs[@]}" --noconfirm --needed
+paru -Syu "${progs[@]}" --noconfirm --needed
 git clone https://github.com/koalagang/suckless-koala.git
 if [ "$HOSTNAME" = 'Alfheim' ]; then
     sudo make install -C suckless-koala/dwm
